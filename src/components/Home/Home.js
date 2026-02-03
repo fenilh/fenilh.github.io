7a15aaa8518459ac858ee924a44c21dd10e3507e
fenilh <your_email@gmail.com>
2026-02-04 01:44:37 +0530

    Fix dependency conflicts for React 18.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -102,16 +102,7 @@ function Home() {
                   <FaLinkedinIn />
                 </a>
               </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li> */}
+             
             </ul>
           </Col>
         </Row>
